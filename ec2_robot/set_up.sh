391c2f75c6ce566c0aa640be71cc948e9137499a
mxcheung <mxcheung@users.noreply.github.com>
2024-12-09 11:16:57 +1100

    Create set_up.sh

---
+++ ec2_robot/set_up.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo yum update -y 
+
+sudo amazon-linux-extras install docker 
+
+sudo yum install docker -y
+
+sudo service docker start 
+
+sudo usermod -a -G docker ec2-user 
+
+sudo yum -y install python3-pip python3-devel nginx git
+
+sudo yum install -y postgresql15
+
+pip install gdown
